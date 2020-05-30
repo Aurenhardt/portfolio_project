4c3c5ba7e0538bb43e83c28dfa14187f7ca3b58f
Ubuntu <ubuntu@ip-172-31-23-182.us-west-1.compute.internal>
2020-05-30 21:09:22 +0000

    Created the overiewpage app in django. Also created basic view functions in views.py and edited urls.py in both the overviewpage app and the django default urls.py to access the views I created.

---
+++ overviewpage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# . means from this directory, this file allows django to view webpages made in overviewpage's views.py
+# views.home  access the home functions in views.py
+urlpatterns = [
+	path('', views.home, name='overviewpage-top'),
+	path('about/', views.about, name='overviewpage-about'),
+]
