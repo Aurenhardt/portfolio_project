45537cb9b532e7303a89c04e42358a46a48f3040
Aurenhardt <ubuntu@ip-172-31-23-182.us-west-1.compute.internal>
2020-06-01 18:44:57 +0000

    Created a generic base.html to allow multiple pages to have shared formats. Reformated about.html and overviewpage.html to use the generic base.html format. Added code in overviewpage.html to to allow it to display multiple entries. Created a main.css to use for all pages since it is loaded in base.html.

--- overviewpage/views.py
+++ overviewpage/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+#Dummy Data to mimic entries in a database to pass into the html.
 
-def home(request): 
-	return render(request,'overviewpage/overviewpage.html')
+posts = [
+	{
+	 'author': 'Aurenhardt',
+	 'title': 'Entry 1',
+	 'content': 'Description 1',
+	 'date_posted': 'June 1, 2020'
+
+	},
+	{
+         'author': 'Aurelius',
+         'title': 'Entry 2',
+         'content': 'Description 2',
+         'date_posted': 'June 2, 2020'
+
+        }
+]
+
+
+def home(request):
+	context = { 
+		'posts': posts 
+	}
+
+#Dictionary that contains a key 'posts' which is connected to the posts list at the beginning.
+ 
+	return render(request,'overviewpage/overviewpage.html', context)
 
 def about(request):
 	return render(request,'overviewpage/about.html')
