1d6ee6ac2cb5d106718330ce0fb3a9444e5e541f
Aurenhardt <ubuntu@ip-172-31-23-182.us-west-1.compute.internal>
2020-06-23 17:54:31 +0000

    Modified database so that each image post now fits under a category.

--- overviewpage/models.py
+++ overviewpage/models.py
@@ -3,10 +3,17 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Post(models.Model):
+
+    POST_TYPE_CHOICES = (("0", "General"), ("1", "Armor Concepts"), ("2", "Portraits"), ("3", "Character Design"),)
+
     title = models.CharField(max_length=100)
     image = models.FileField(null=True, blank=True)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User,on_delete=models.CASCADE) #if user is deleted, delete post as well.
-    
+    PostCategory = models.CharField(max_length = 20, choices = POST_TYPE_CHOICES, default = '0')
+
+    def post_districtList(self):
+        return dict(Post.POST_TYPE_CHOICES)[self.PostCategory]
